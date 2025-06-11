fbfac9ac97328dedf93c9ebdee9b0d6f25a1c0f4
lhnhidev <lhnhi420@gmail.com>
2025-06-11 22:37:02 +0700

    service component

--- Frontend/src/components/ServiceComponent.vue
+++ Frontend/src/components/ServiceComponent.vue
@@ -1,5 +1,58 @@
 <template>
-  <div></div>
+  <div class="flex justify-center gap-28 px-12 py-20">
+    <div
+      class="space-y-2 text-center transition hover:text-[var(--primary-color)]"
+    >
+      <div class="text-4xl">
+        <FontAwesomeIcon :icon="faTruckFast" />
+      </div>
+
+      <h2 class="text-2xl font-bold uppercase">Giao hàng mọi nơi</h2>
+
+      <p>Phục vụ mọi lúc, mọi nơi</p>
+    </div>
+    <div
+      class="space-y-2 text-center transition hover:text-[var(--primary-color)]"
+    >
+      <div class="text-4xl">
+        <FontAwesomeIcon :icon="faPiggyBank" />
+      </div>
+
+      <h2 class="text-2xl font-bold uppercase">Cam kết chất lượng</h2>
+
+      <p>Hỗ trợ đổi trả sách nếu có lỗi in ấn</p>
+    </div>
+    <div
+      class="space-y-2 text-center transition hover:text-[var(--primary-color)]"
+    >
+      <div class="text-4xl">
+        <FontAwesomeIcon :icon="faTicket" />
+      </div>
+
+      <h2 class="text-2xl font-bold uppercase">Ưu đãi và giảm giá</h2>
+
+      <p>Phục vụ mọi lúc, mọi nơi</p>
+    </div>
+    <div
+      class="space-y-2 text-center transition hover:text-[var(--primary-color)]"
+    >
+      <div class="text-4xl">
+        <FontAwesomeIcon :icon="faPhoneVolume" />
+      </div>
+
+      <h2 class="text-2xl font-bold uppercase">Giao hàng mọi nơi</h2>
+
+      <p>Phục vụ mọi lúc, mọi nơi</p>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import {
+  faPhoneVolume,
+  faPiggyBank,
+  faTicket,
+  faTruckFast,
+} from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+</script>
--- Frontend/src/views/client/HomePage.vue
+++ Frontend/src/views/client/HomePage.vue
@@ -1,9 +1,11 @@
 <template>
   <HeaderComponent />
   <SliderComponent />
+  <ServiceComponent />
 </template>
 
 <script setup>
 import SliderComponent from "@components/SliderComponent.vue"
 import HeaderComponent from "@components/HeaderComponent.vue"
+import ServiceComponent from "@components/ServiceComponent.vue"
 </script>
