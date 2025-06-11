143b5d12eae107944161536759d7e5b3899c44f9
lhnhidev <lhnhi420@gmail.com>
2025-06-11 21:17:04 +0700

    header component

--- Frontend/src/components/HeaderComponent.vue
+++ Frontend/src/components/HeaderComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <header class="w-full">
+  <header class="sticky top-[-44px] w-full shadow-sm">
     <div
       class="flex justify-between bg-[var(--bg-black-color)] px-4 py-3 text-sm text-gray-300"
     >
@@ -18,48 +18,68 @@
     </div>
 
     <div class="flex justify-between bg-white px-4">
-      <div class="flex w-64 items-center">
-        <img src="/logo.svg" alt="CTULib logo" />
-      </div>
+      <a href="#" class="flex w-72 items-center">
+        <img src="/logomobile.png" alt="CTULib_logo" />
+      </a>
 
-      <nav class="flex">
+      <nav class="flex items-center">
         <div>
-          <a href="#" class="block px-5 py-8">Books</a>
+          <a href="#" class="block px-5 py-4 hover:text-[var(--primary-color)]"
+            >Trang chủ</a
+          >
         </div>
         <div>
-          <a href="#" class="block px-5 py-8">Categorise</a>
+          <a href="#" class="block px-5 py-4 hover:text-[var(--primary-color)]"
+            >Sách</a
+          >
         </div>
         <div>
-          <a href="#" class="block px-5 py-8">Religion</a>
+          <a href="#" class="block px-5 py-4 hover:text-[var(--primary-color)]"
+            >Ebook</a
+          >
         </div>
         <div>
-          <a href="#" class="block px-5 py-8">Products</a>
+          <a href="#" class="block px-5 py-4 hover:text-[var(--primary-color)]"
+            >Lịch sử</a
+          >
         </div>
         <div>
-          <a href="#" class="block px-5 py-8">Historical</a>
+          <a href="#" class="block px-5 py-4 hover:text-[var(--primary-color)]"
+            >Tin tức</a
+          >
         </div>
         <div>
-          <a href="#" class="block px-5 py-8">Contact Us</a>
+          <a href="#" class="block px-5 py-4 hover:text-[var(--primary-color)]"
+            >Giới thiệu</a
+          >
         </div>
         <div>
-          <a href="#" class="block px-5 py-8">News</a>
+          <a href="#" class="block px-5 py-4 hover:text-[var(--primary-color)]"
+            >Liên hệ</a
+          >
         </div>
       </nav>
 
-      <div>
-        <div>
-          <a href="#">
+      <div class="flex gap-5">
+        <div class="flex items-center">
+          <a href="#" class="text-2xl hover:text-[var(--primary-color)]">
             <FontAwesomeIcon :icon="faMagnifyingGlass" />
           </a>
         </div>
-        <div>
-          <a href="#"></a>
+        <div class="flex items-center">
+          <a href="#" class="text-2xl hover:text-[var(--primary-color)]">
+            <FontAwesomeIcon :icon="faUser" />
+          </a>
         </div>
-        <div>
-          <a href="#"></a>
+        <div class="flex items-center">
+          <a href="#" class="text-2xl hover:text-[var(--primary-color)]">
+            <FontAwesomeIcon :icon="faHeart" />
+          </a>
         </div>
-        <div>
-          <a href="#"></a>
+        <div class="flex items-center">
+          <a href="#" class="text-2xl hover:text-[var(--primary-color)]">
+            <FontAwesomeIcon :icon="faBagShopping" />
+          </a>
         </div>
       </div>
     </div>
@@ -68,5 +88,11 @@
 
 <script setup>
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons"
+import {
+  faMagnifyingGlass,
+  faBagShopping,
+  faUser,
+  faHeart,
+} from "@fortawesome/free-solid-svg-icons"
+// import {} from "@fortawesome/free-regular-svg-icons"
 </script>
