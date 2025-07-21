d446f25793d54f3bd11886ab468cc7728ed955b6
lhnhidev <lhnhi420@gmail.com>
2025-07-22 00:19:18 +0700

    manage books in admin

--- Backend/src/controller/books.controller.js
+++ Backend/src/controller/books.controller.js
@@ -12,6 +12,20 @@ class BooksController {
     }
     res.json(await Books.findOne({ maSach: id }));
   }
+
+  async getBookPag(req, res) {
+    const { start, end } = req.query;
+    const startIndex = parseInt(start);
+    const endIndex = parseInt(end);
+
+    const limit = endIndex - startIndex + 1;
+
+    const docs = await Books.find()
+      .skip(startIndex - 1)
+      .limit(limit);
+
+    res.json(docs);
+  }
 }
 
 module.exports = new BooksController();
--- Backend/src/router/books.routes.js
+++ Backend/src/router/books.routes.js
@@ -3,6 +3,7 @@ const route = express.Router();
 const booksController = require("../controller/books.controller");
 
 route.get("/", booksController.index);
+route.get("/page", booksController.getBookPag);
 route.get("/:id", booksController.showBookDetail);
 
 module.exports = route;
